6aec20ad21478753d8155ad0a5fc606d4767e9b5
rana <rana.buddy@gmail.com>
2023-12-01 22:51:11 +0600

    feat:updated

---
+++ middleware/ratelimiter.js
@@ -0,0 +1,27 @@
+import setRateLimit from 'express-rate-limit';
+
+
+
+
+  
+  const rateLimitMiddleware = (req, res, next) => {
+  
+   setRateLimit({
+        windowMs: 1 * 60 * 1000, 
+        max: 2,
+        message: "Hold on their, maybe get a life instead of spamming my api.",
+        standardHeaders: true,
+        legacyHeaders: true, 
+        skipFailedRequests: true
+      });
+    
+          next();
+   
+  
+  }
+  
+  
+  export default rateLimitMiddleware
+  
+  
+  
