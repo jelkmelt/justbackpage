ff7d401cb3050d4bac57acb8d9b2e97111802b2c
rana <rana.buddy@gmail.com>
2023-12-03 22:20:05 +0600

    feat:cloudinary delete api added

--- utils/cloudnaryImgProcess.js
+++ utils/cloudnaryImgProcess.js
@@ -1,6 +1,7 @@
-import cloudinary from ('cloudinary').v2;
+import { v2 as cloudinary } from 'cloudinary'
 import dotenv from "dotenv";
 dotenv.config();
+
 cloudinary.config({
   cloud_name: process.env.cloud_name,
   api_key: process.env.api_key,
